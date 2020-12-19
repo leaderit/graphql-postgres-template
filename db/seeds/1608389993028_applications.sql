0125bac84c2ad2813da75f9ef0528ac2558fd38b
Valerii Grazhdankin <ias-projects@mail.ru>
2020-12-19 20:01:06 +0500

    add applications table
    add applications table and test seeds

---
+++ db/seeds/1608389993028_applications.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.applications (id, client_id, client_secret) VALUES ('7382848c-41e3-11eb-8e8c-97d82c2714b2', 'ios-v1', 'supersecret');
+INSERT INTO public.applications (id, client_id, client_secret) VALUES ('9d5313a2-41e4-11eb-879f-27dfc547d010', 'android-v1', 'supersecret');
+INSERT INTO public.applications (id, client_id, client_secret) VALUES ('b22fb8a2-41e4-11eb-950c-5f4f5512824b', 'webapp-v1', 'supersecret');
