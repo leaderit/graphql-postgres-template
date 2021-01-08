fc9175eb065ff320cae1b170e2a5206ba7143464
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-09 04:48:55 +0500

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,6 +2,10 @@ module.exports = [{
     script  : 'server.js',
     name    : 'server',
     watch   : true,
+    ignore_watch: [
+        'public',
+        'node_modules'
+    ]
     // exec_mode: 'cluster',
     // instances: 2
 }]
