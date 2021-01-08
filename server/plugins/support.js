0ac2c512b12ffc46810589fb74df3d39b8214482
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-09 02:40:31 +0500

    Update support.js

--- server/plugins/support.js
+++ server/plugins/support.js
@@ -6,26 +6,7 @@ const fp = require('fastify-plugin')
 // to export the decorators to the outer scope
 
 module.exports = fp(function (fastify, opts, next) {
-
-  // Обработка освобождения событий от Redis
-	fastify.decorate( 'onRedisEvent', ( channel, message, data ) => {
-    console.log( 'REDIS EVENT:', {channel, message, data } )
-    // const event = message.split(':')[1]
-    // const key = data.split('/')
-    // if ( fastify.redisKey('reservgroup') === key[0]+'/'+key[1] ) {
-    //   const groupid = key[2]
-    //   const landid = key[3]
-    //   console.log('Free Reserv %s/%s', groupid, landid)
-    //   // Известить об освобождении резерва всех подписчиков группы
-    //   fastify.emitGroup('free', groupid, { 
-    //     land: { 
-    //       groupid, 
-    //       landid 
-    //     } 
-    //   })
-    // }
-  })
-
+  fastify.decorate('support_ok', true )
   next()
 })
 
