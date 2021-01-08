19baa029e79b592d486ae90836e61f8264975aae
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-09 04:50:03 +0500

    Update root.js

--- server/services/root.js
+++ server/services/root.js
@@ -11,50 +11,6 @@ module.exports = function (fastify, opts, next) {
     reply.send({ greeting: 'Hello' })
   })
 
-  // fastify.post('/api0/graphql', async function (request, reply) {
-  //   const { hasura } = fastify
-
-  //   if ( hasura ) {
-  //     console.log( request.body )
-  //     const { data } = await hasura('', 
-  //       request.body,
-  //       {
-  //         headers: {
-  //           'X-Hasura-Login': '', //user.username,
-  //           'X-Hasura-Password': '', //user.password
-  //         }
-  //       })
-  //     console.log( 'hasura data = ', data )
-  //     reply.send( data )
-  //   }
-  // })
-
-  // fastify.get('/api0/auth/hasura', function (request, reply) {
-
-  //   // const { req } = this
-  //   // const { token } = this
-
-  //   console.log( 'HASURA AUTH', request.headers ) //, this )
-  //   // const user = sessions.tokens[ token ] || null
-  //   // if ( user ) {
-  //   //     console.log( 'User found', user)
-  //   //     return { 
-  //   //         "X-Hasura-User-Id": user.id,
-  //   //         "X-Hasura-Role": user.userRole.name,
-  //   //         "X-Hasura-Org": user.org_id || '' 
-  //   //     }
-  //   // }
-  //   //res.statusCode = 401
-  //   //console.log( 'user not found', { token })
-  //   reply.send({ 
-  //       "X-Hasura-User-Id": "",
-  //       "X-Hasura-Role": "anonymous",
-  //       // "X-Hasura-Login": req.headers['x-hasura-login'] || '',
-  //       // "X-Hasura-Password": req.headers['x-hasura-password'] || ''
-  //   })
-  //   // reply.send({ root: true })
-  // })  
-
   next()
 }
 
