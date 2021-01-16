f02f0adc7dd56be9e1f1ba0194b0931402e3fbb5
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-16 14:53:47 +0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -54,10 +54,17 @@ const config = {
 
   // Путь к файловому хранилищу с правами доступа
   storage: {
-    root: process.env['STORAGE'] || path.join(process.env['PWD'], 'storage'),
-    url: '/storage',
+    // root: process.env['STORAGE'] || path.join(process.env['PWD'], 'storage'),
+    // url: '/storage',
     cacheKey:'files/',
-    cacheLife:60
+    cacheLife:60,
+
+    // min.io
+    endPoint: 'storage',
+    port: 9000,
+    useSSL: false,
+    accessKey: process.env['STORAGE_USER'],
+    secretKey: process.env['STORAGE_SECRET']
   },
 
   // Локальные хранилища файлов
