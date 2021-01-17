fbe86f4d1135e88b4b62a988c98d11510ac453a3
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-17 21:39:52 +0500

    Update support.js

--- server/plugins/support.js
+++ server/plugins/support.js
@@ -7,6 +7,15 @@ const fp = require('fastify-plugin')
 
 module.exports = fp(function (fastify, opts, next) {
   fastify.decorate('support_ok', true )
+
+  // fastify.decorate('checkCode', async function(user, action, code) {
+  //   console.log('Check code', { user: user.id, action, code })
+  //   return true;
+  // })
+  // fastify.decorate('sendCode', async function(user, action, code) {
+  //   console.log('send code', { user: user.id, action, code })
+  //   return true;
+  // })
   next()
 })
 
