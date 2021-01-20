67f3f31348b33bd35c22eff05e70582e5914aa26
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-20 14:34:31 +0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,15 +1,14 @@
 # !/bin/sh
+# Set project name to current directory name
+PROJECT=$(pwd | grep -o '[^/]*$')
 
-PROJECT=my-project
-
-# Prepare your project directory
-# mkdir my-project
-# cd my-project 
-# git clone https://github.com/leaderit/graphql-postgres-template.git .
+git clone https://github.com/leaderit/graphql-postgres-template.git .
 
 cp env-example .env
 cp server/env-example server/.env
 # EDIT .env  and set NAME variable to "my-project"
+sed -i "s/NAME=.*/NAME=$NAME/" .env 
+
 # set other variables in apropriate values, espetialy passwords
 cd server
 npm install
@@ -17,3 +16,10 @@ cd ..
 docker-compose up -d
 
 # database and metadata
+./hasura-cli migrate apply
+./hasura-cli metadata apply
+./hasura-cli seeds apply
+
+# Run tests
+./run-tests
+
