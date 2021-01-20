5f319275c836d8e812d11ece4228dfc006ad1cdf
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-20 23:14:37 +0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,21 @@
 # !/bin/sh
 C_NORMAL="\x1B[0m"
+C_RED="\x1B[31m"
 C_GREEN="\x1B[32m"
+C_YELLOW="\x1B[33m"
+C_BLUE="\x1B[34m"
+C_MAGENTA="\x1B[35m"
+C_CYAN="\x1B[36m"
+C_WHITE="\x1B[37m"
+# 
+KNRM="\x1B[0m"
+KRED="\x1B[31m"
+KGRN="\x1B[32m"
+KYEL="\x1B[33m"
+KBLU="\x1B[34m"
+KMAG="\x1B[35m"
+KCYN="\x1B[36m"
+KWHT="\x1B[37m"
 
 # Set project name to current directory name
 PROJECT=$(pwd | grep -o '[^/]*$')
