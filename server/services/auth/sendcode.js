0d48faee8ffe07b5c92b41305792dc8045e8b4af
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-22 01:11:39 +0500

    Update sendcode.js

--- server/services/auth/sendcode.js
+++ server/services/auth/sendcode.js
@@ -2,13 +2,13 @@ const fp = require('fastify-plugin')
 const crypto = require('crypto')
 
 // Запрос кода подтверждения
-async function sendCode(user, operation, code ){
+async function sendCode(user, action, code ){
     // const { redis } = fastify
     // const { codeLife } = fastify
     // const { createToken } = fastify
     // const { user } = request
 
-    console.log( { user, operation, code } )
+    console.log('SEND CODE: ', { user, action, code } )
     return { 
         code,
     }
