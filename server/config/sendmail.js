759ab99bc934db022966bed59613e4c141afba8d
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-26 22:39:57 +0300

    Update sendmail.js

--- server/config/sendmail.js
+++ server/config/sendmail.js
@@ -1,11 +1,11 @@
 module.exports = {
   disable: true,
-  host: "smtp.mail.ru",
+  host: process.env['SENDMAIL_SERVER'] || "smtp.mail.ru",
   port: 465,
   secure: true, // use TLS
   auth: {
-    user: "user@mail.ru",
-    pass: "bigbigpassword"
+    user: process.env['SENDMAIL_USER'] || 'user@mail.ru',
+    pass: process.env['SENDMAIL_PASSWORD'] || "bigbigpassword"
   },
   tls: {
     // do not fail on invalid certs
