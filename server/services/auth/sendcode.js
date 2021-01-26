1a4136a0a0e44a45a0a5477d903fcad2710abf0a
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-26 22:38:11 +0300

    Update sendcode.js

--- server/services/auth/sendcode.js
+++ server/services/auth/sendcode.js
@@ -2,19 +2,26 @@ const fp = require('fastify-plugin')
 const crypto = require('crypto')
 
 // Запрос кода подтверждения
-async function sendCode(user, action, code ){
-    // const { redis } = fastify
-    // const { codeLife } = fastify
-    // const { createToken } = fastify
-    // const { user } = request
-
-    console.log('SEND CODE: ', { user, action, code } )
+const sendCode = async function (user, action, code){
+    const fastify = this
+    const { sendMail } = fastify
+    const { config } = fastify
+    console.log( config.mail.admin )
+    sendMail({
+      from: config.mail.admin.from,     // адрес отправителя: '"Valeriy Grazhdankin" <ias-projects@mail.ru>'
+      to: 'ias-projects@mail.ru',       // Адреса получателей: "ias-projects@mail.ru, admin@centerstruve.com"
+      subject: 'Confirmation code',     // Строка темы
+      text: 'Code: '+code,              // Plain text body
+      html :"<b>Code: "+code+"</b>"        // Html body
+    })
     return { 
         code,
     }
 }
 
 module.exports = fp(function (fastify, opts, next) {
+    // const apiMethod = api[method].bind( context )
+    sendCode.bind( fastify )
     fastify.decorate ( 'sendCode', sendCode )
     next()
 })
