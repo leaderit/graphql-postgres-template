a519462b03a4288470c6a0ec800d328049ce8eb6
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-27 13:25:48 +0300

    Update sendcode.js

--- server/services/auth/sendcode.js
+++ server/services/auth/sendcode.js
@@ -1,12 +1,17 @@
+// EXAMPLE FOR EMAIL CHANNEL (LOGIN=VALID EMAIL ADDRESS)
+// Send an authorisation code for the user via second chennel
 const fp = require('fastify-plugin')
 const crypto = require('crypto')
 
-// Запрос кода подтверждения
+// Send Authorisation code
 const sendCode = async function (user, action, code){
     const fastify = this
     const { sendMail } = fastify
     const { config } = fastify
+    // Delay before send code 
+    await new Promise(resolve => setTimeout(resolve, config.auth.codeSendDelay * 1000 ));
     console.log( config.mail.admin )
+    // awat 
     sendMail({
       from: config.mail.admin.from,     // адрес отправителя: '"Valeriy Grazhdankin" <ias-projects@mail.ru>'
       to: 'ias-projects@mail.ru',       // Адреса получателей: "ias-projects@mail.ru, admin@centerstruve.com"
