2cd57de0a3f29caa279b9ff66d935eca4fb1da86
Valerii Grazhdankin <ias-projects@mail.ru>
2021-01-27 13:25:22 +0300

    Update auth.js

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,9 +1,17 @@
-// Параметры авторизации
+// Authorisation parameters
 module.exports = {
-  tokenLife : 3600, // Время жизни сессии в секундах
+  tokenLife : 3600, // Authorisation token life time in seconds
   // Если обновление жетона (токена) производится менее чем за минимальное время жизни
   // возвращается тот же жетон (токен)
   minTokenLife : 15,
   deleteSessionTimeout: 5,
-  codeLife: 240 // Время жизни кода подтверждения в секундах
+  // Authorisation code security section
+  codeLife: 240, // Authorisation code life time in seconds
+  codeSendDelay: 1, // Delay before send code in seconds
+  codeCheckDelay: 5, // delay before check code in seconds
+  actions: [
+    'login',
+    'register',
+    'unregister'
+  ]
 }
