0501476770c404df7359a4a5a00e40691b215270
Valerii Grazhdankin <ias-projects@mail.ru>
2021-02-24 00:55:50 -0500

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = [{
     script  : 'server.js',
     name    : 'server',
     watch   : true,
+    // max_memory_restart: '4G',
     ignore_watch: [
         'public',
         'node_modules',
