985781501788d3fa02be35ea93398bf25741f3d6
Valerii Grazhdankin <ias-projects@mail.ru>
2021-02-24 20:46:10 -0500

    Update io.js
    add storage_rmfile, storage_mvfile, storage_cpfile entries

--- server/services/s3/io.js
+++ server/services/s3/io.js
@@ -1,35 +1,35 @@
 'use strict'
 const fs = require('fs')
-const pump = require('pump')
+// const pump = require('pump')
 
-var file = '/storage/tmp/vsms-ht-logo.mp4'
+// var file = '/storage/tmp/vsms-ht-logo.mp4'
 
-async function start(fastify, request, reply) {
-  const storage = fastify.s3storage
-    // Make a bucket called europetrip.
-    try {
-        await storage.makeBucket('users1', 'Orenburg');
-    } catch (err) {
-        // console.log(err)
-    }
+// async function start(fastify, request, reply) {
+//   const storage = fastify.s3storage
+//     // Make a bucket called europetrip.
+//     try {
+//         await storage.makeBucket('users1', 'Orenburg');
+//     } catch (err) {
+//         // console.log(err)
+//     }
 
-    var metaData = {
-        // 'Content-Type': 'video/mov',
-        'Content-Type': 'video/mp4',
-        // 'Content-Type': 'application/octet-stream',
-        'X-Amz-Meta-Testing': 1234,
-        'example': 5678
-    }
-    // Using fPutObject API upload your file to the bucket europetrip.
-    const etag = await storage.fPutObject('users1', 'video/81fc512a-4236-11eb-96d1-975577b0463c', file, metaData)
-    // , function(err, etag) {
-    // // storage.fPutObject('users', 'config.js', file, metaData, function(err, etag) {
-    // if (err) return console.log(err)
-    // console.log('File uploaded successfully. etag=', etag)
-    // });
-    console.log('File uploaded successfully. etag=', etag)
+//     var metaData = {
+//         // 'Content-Type': 'video/mov',
+//         'Content-Type': 'video/mp4',
+//         // 'Content-Type': 'application/octet-stream',
+//         'X-Amz-Meta-Testing': 1234,
+//         'example': 5678
+//     }
+//     // Using fPutObject API upload your file to the bucket europetrip.
+//     const etag = await storage.fPutObject('users1', 'video/81fc512a-4236-11eb-96d1-975577b0463c', file, metaData)
+//     // , function(err, etag) {
+//     // // storage.fPutObject('users', 'config.js', file, metaData, function(err, etag) {
+//     // if (err) return console.log(err)
+//     // console.log('File uploaded successfully. etag=', etag)
+//     // });
+//     console.log('File uploaded successfully. etag=', etag)
 
-}
+// }
 
 async function storage_newfile(fastify, args) {
   const storage = fastify.s3storage
@@ -59,18 +59,51 @@ async function storage_fileurl(fastify, args) {
   }
 }
 
+
+async function storage_new(fastify, args) {
+  const storage = fastify.s3storage
+  let success = false
+  let error = null
+
+  try {
+    await storage.makeBucket(args.storage, args.region )
+    success = true
+  } catch (err) {
+    error = err.name +':'+err.message
+  }
+  return {
+    success,
+    error
+  }
+}
+
+async function storage_rm(fastify, args) {
+  const storage = fastify.s3storage
+  let success = false
+  let error = null
+  try {
+    await storage.removeBucket(args.storage)
+    success = true
+  } catch (err) {
+    error = err.name +':'+err.message
+  }
+  return {
+    success,
+    error
+  }
+}
+
 async function listBuckets( fastify )
 {
   const storage = fastify.s3storage
   var list = []
-    console.log('LIST BUCKETS')
     try {
         list = await storage.listBuckets();
-        // console.log( list )
-        return list
+        // return list
     } catch (err) {
         console.log(err)
     }
+    return list
 }
 
 async function listFiles( fastify, args )
@@ -106,19 +139,79 @@ async function listFiles( fastify, args )
   return promise
 }
 
+async function storage_rmfile(fastify, args) {
+  const storage = fastify.s3storage
+  let success = false
+  let error = null
+  try {
+    await storage.removeObject(args.storage, args.path+args.name)
+    success = true
+  } catch (err) {
+    error = err.name +':'+err.message
+  }
+  return {
+    success,
+    error
+  }
+}
+
+async function storage_cpfile(fastify, args) {
+  const storage = fastify.s3storage
+  const { Minio } = fastify
+  let success = false
+  let error = null
+  var conds = new Minio.CopyConditions()
+
+  try {
+    await storage.copyObject(args.to_storage, args.to_path+args.to_name, '/'+args.storage+'/'+args.path+args.name, conds )
+    success = true
+  } catch (err) {
+    error = err.name +':'+err.message
+  }
+  return {
+    success,
+    error
+  }
+}
+
+async function storage_mvfile(fastify, args) {
+  const storage = fastify.s3storage
+  const { Minio } = fastify
+  let success = false
+  let error = null
+  var conds = new Minio.CopyConditions()
+  
+  try {
+    await storage.copyObject(args.to_storage, args.to_path+args.to_name, '/'+args.storage+'/'+args.path+args.name, conds )
+    await storage.removeObject(args.storage, args.path+args.name)
+    success = true
+  } catch (err) {
+    error = err.name +':'+err.message
+  }
+  return {
+    success,
+    error
+  }
+}
+
 async function s3io( fastify, request, reply ){
   // console.log( request.headers )
-  console.log( request.body )
+  // console.log( request.body )
   const session_vars = request.body.session_variables || {}
   const args = request.body.input || {}
   const action = request.body.action.name
   // const action = args.action
-  console.log( action )
+  // console.log( action )
 
   if ( action === 'storages') return await listBuckets(fastify)
   if ( action === 'storage_files') return await listFiles(fastify, args )
   if ( action === 'storage_newfile') return await storage_newfile(fastify, args )
   if ( action === 'storage_fileurl') return await storage_fileurl(fastify, args )
+  if ( action === 'storage_new') return await storage_new(fastify, args )
+  if ( action === 'storage_rm') return await storage_rm(fastify, args )
+  if ( action === 'storage_rmfile') return await storage_rmfile(fastify, args )
+  if ( action === 'storage_mvfile') return await storage_mvfile(fastify, args )
+  if ( action === 'storage_cpfile') return await storage_cpfile(fastify, args )
 
   return []
 }
