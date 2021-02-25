c1883acee47270b801e55a0b5c9df4b5e228b990
Valerii Grazhdankin <ias-projects@mail.ru>
2021-02-24 20:45:18 -0500

    Update storage.gql.js

--- server/gql/storage.gql.js
+++ server/gql/storage.gql.js
@@ -2,14 +2,40 @@
 module.exports = {
     ///
     storages: 
-        `
-        query StorageFiles {
-            storages {
-            creationDate
-            name
-            }
-        }      
-        `,
+      `
+      query StorageFiles {
+          storages {
+          creationDate
+          name
+          }
+      }      
+      `,
+    ///
+    newStorage:
+    `
+      mutation NewStorage(
+          $storage: String!, 
+          $region: String!
+        ) {
+        storage_new(
+          region: $region, 
+          storage: $storage
+        ) {
+          error
+          success
+        }
+      }
+    `,
+    ///
+    removeStorage:
+    `
+      mutation MyMutation($storage: String!) {
+        storage_rm(storage: $storage) {
+          error
+          success
+        }
+      }    
+    `,
     ///
     files:
       `
@@ -43,20 +69,34 @@ module.exports = {
       
       `,
     ///
-      fileurl:
-        `
-        query StorageFileUrl(
-                $path: String! = "", 
-                $name: String! = "", 
-                $storage: String! = ""
-            ) {
-            storage_fileurl(path: $path, storage: $storage, name: $name) {
-                url
-            }
-        }            
-        `
+    fileUrl:
+      `
+      query StorageFileUrl(
+              $path: String! = "", 
+              $name: String! = "", 
+              $storage: String! = ""
+          ) {
+          storage_fileurl(path: $path, storage: $storage, name: $name) {
+              url
+          }
+      }            
+      `,
     ///
-    }
+    removeFile: 
+      `
+      mutation RemoveFile(
+        $name: String!, 
+        $path: String = "", 
+        $storage: String!
+      ) {
+        storage_rmfile(storage: $storage, path: $path, name: $name) {
+          error
+          success
+        }
+      }        
+      `,
+
+}
     
     
     
