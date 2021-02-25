3c7888afaf1c712cbd3fa682c765440bd2b3109c
Valerii Grazhdankin <ias-projects@mail.ru>
2021-02-24 20:45:24 -0500

    Update minio.js

--- server/plugins/minio.js
+++ server/plugins/minio.js
@@ -1,10 +1,12 @@
 'use strict'
-const Storage = require('minio')
+const Minio = require('minio')
 const fp = require('fastify-plugin')
 
 module.exports = fp(function (fastify, opts, next ) {
     opts.storage = opts.storage || {}
-    var s3storage = new Storage.Client( opts.storage )
+    var s3storage = new Minio.Client( opts.storage )
     fastify.decorate('s3storage', s3storage)
+    fastify.decorate('Minio', Minio)
+
     next()
 })
