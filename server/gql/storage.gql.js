f2dca90e89c33d833b4becf8bc4a66dc04b68ed5
Valerii Grazhdankin <ias-projects@mail.ru>
2021-02-24 20:53:35 -0500

    Update storage.gql.js

--- server/gql/storage.gql.js
+++ server/gql/storage.gql.js
@@ -95,7 +95,54 @@ module.exports = {
         }
       }        
       `,
-
+    ///
+    copyFile:
+    `
+      mutation CopyFile(
+        $storage: String!, 
+        $path: String = "", 
+        $name: String!,
+        $to_storage: String!,
+        $to_path: String = "", 
+        $to_name: String!
+      ) {
+        storage_cpfile(
+          storage: $storage, 
+          path: $path,
+          name: $name, 
+          to_storage: $to_storage, 
+          to_path: $to_path, 
+          to_name: $to_name
+        ) {
+          error
+          success
+        }
+      }    
+    `,      
+    ///
+    moveFile:
+    `
+      mutation MoveFile(
+        $storage: String!, 
+        $path: String = "", 
+        $name: String!,
+        $to_storage: String!,
+        $to_path: String = "", 
+        $to_name: String!
+      ) {
+        storage_mvfile(
+          storage: $storage, 
+          path: $path,
+          name: $name, 
+          to_storage: $to_storage, 
+          to_path: $to_path, 
+          to_name: $to_name
+        ) {
+          error
+          success
+        }
+      }    
+    `,
 }
     
     
