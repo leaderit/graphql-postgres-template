bd2ab8ff9a98b031b97ec3a6dd25708ba61df04d
Valerii Grazhdankin <ias-projects@mail.ru>
2021-03-03 09:34:37 +0300

    Update sendcode.js

--- server/services/auth/sendcode.js
+++ server/services/auth/sendcode.js
@@ -3,22 +3,41 @@
 const fp = require('fastify-plugin')
 const crypto = require('crypto')
 
+const emailFormat=/^[a-zA-Z0-9_.\-+]+(?<!^[0-9]*)@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+const phoneFormat=/^(?:\+?\d{1,2}[ -]?[\d -][\d -]+)$/
+
 // Send Authorisation code
 const sendCode = async function (user, action, code){
     const fastify = this
     const { sendMail } = fastify
     const { config } = fastify
+
     // Delay before send code 
     await new Promise(resolve => setTimeout(resolve, config.auth.codeSendDelay * 1000 ));
-    // console.log( config.mail.admin )
-    // awat 
-    sendMail({
-      from: config.mail.admin.from,     // адрес отправителя: '"Valeriy Grazhdankin" <ias-projects@mail.ru>'
-      to: 'ias-projects@mail.ru',       // Адреса получателей: "ias-projects@mail.ru, admin@centerstruve.com"
-      subject: 'Confirmation code',     // Строка темы
-      text: 'Code: '+code,              // Plain text body
-      html :"<b>Code: "+code+"</b>"        // Html body
-    })
+
+    var email = user.email
+    var phone = user.phone
+    if ( emailFormat.test(user.name) ) email = user.name
+    if ( phoneFormat.test(user.name) ) phone = user.phone
+
+    // console.log( { user, email, phone })
+
+    if ( emailFormat.test(email) ) {
+        // send a code via the email
+        sendMail({
+            from: config.mail.admin.from,   // A Sender Address: '"Sender Name" <e-mail@domain.com>'
+            to: email,                      // Receiver adresses^ comma separated: "addr1@mail.com, addr2@mail.com"
+            subject: 'Confirmation code',   // Text of the topic
+            text: 'Code: '+code,            // Plain text body
+            html :"<b>Code: "+code+"</b>"   // Html body
+        })      
+    }
+
+    if ( phoneFormat.test( phone) ) {
+        // send code via phone/messanger etc
+
+    }
+
     return { 
         code,
     }
